90383a1609fca2e4f7f337f3132e09be550d9ba5
Aidablaya <you@example.com>
2024-05-28 13:37:47 +0200

    add login & logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,6 @@ import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 import { createAccesToken } from '../libs/jwt.js';
 
-
 export const register = async (req, res) => {
     const {email, password, username} = req.body;
     
@@ -23,7 +22,7 @@ export const register = async (req, res) => {
         const userSaved = await newUser.save();
         
         //4 se crea el token
-        const token = await createAccesToken({id: userSaved.id})
+        const token = await createAccesToken({id: userSaved._id})
 
         //guardar en cookie y responder en frontend
         res.cookie('token', token);
@@ -41,4 +40,40 @@ export const register = async (req, res) => {
     }
   
 };
-export const login = (req, res) => res.send("login");
+
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    
+    try {
+        //buscar usuario
+        const userFound = await User.findOne({email});
+        if (!userFound) return res.status(400).json({message: "User not found"});
+        //coincide?
+        const isMatch = await bcrypt.compare(password, userFound.password);
+        if (!isMatch) return res.status(400).json({message: "Incorrect password"});
+    
+
+        const token = await createAccesToken({id: userFound._id})
+
+        res.cookie("token", token);
+        
+        res.json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+            createdAt: userFound.createdAt,
+            updatedAt: userFound.updatedAt,
+        });
+
+    }   catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+  
+};
+
+export const logout = (req, res) => {
+    res.cookie('token', "", {
+        expires: new Date(0),
+    })
+    return res.sendStatus(200)
+}
